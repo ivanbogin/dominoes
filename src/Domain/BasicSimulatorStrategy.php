693e67c9c04720298a6081102a6dc84e99dca629
Ivan <ivanbogin@gmail.com>
2020-11-15 23:50:51 +0100

    testPlayerTurnAvailableMove success

--- src/Domain/BasicSimulatorStrategy.php
+++ src/Domain/BasicSimulatorStrategy.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dominoes\Domain;
 
+use function sprintf;
+
 class BasicSimulatorStrategy implements SimulatorStrategy
 {
     protected Logger $logger;
@@ -15,6 +17,42 @@ class BasicSimulatorStrategy implements SimulatorStrategy
 
     public function playerTurn(Player $player, Dominoes $dominoes): void
     {
+        $playerPile     = $player->getHandPile();
+        $boardPile      = $dominoes->getBoardPile();
+        $boardLeftSide  = $boardPile->getLeftSide();
+        $boardRightSide = $boardPile->getRightSide();
 
+        foreach ($playerPile->getTiles() as $tile) {
+            // board right side
+            if ($tile->getRightSide() === $boardLeftSide) {
+                $playerPile->removeTile($tile);
+                $boardPile->addLeftTile($tile);
+                $this->logMovement($player->getName(), $tile, $dominoes->getBoardPile()->getLeftTile());
+
+                return;
+            }
+
+            // board left side
+            if ($tile->getLeftSide() === $boardRightSide) {
+                $playerPile->removeTile($tile);
+                $boardPile->addTile($tile);
+                $this->logMovement($player->getName(), $tile, $dominoes->getBoardPile()->getRightTile());
+
+                return;
+            }
+        }
+        // nothing found, get from stock and try again
+    }
+
+    protected function logMovement($playerName, $playerTile, $boardTile): void
+    {
+        $this->logger->addLog(
+            sprintf(
+                '%s plays %s to connect to tile %s on the board',
+                $playerName,
+                $playerTile,
+                $boardTile
+            )
+        );
     }
 }
