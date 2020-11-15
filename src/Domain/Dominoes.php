d9cfcc23442c93417a6997eeb55d9afd83fec423
Ivan <ivanbogin@gmail.com>
2020-11-15 18:48:39 +0100

    generateTiles

--- src/Domain/Dominoes.php
+++ src/Domain/Dominoes.php
@@ -6,10 +6,20 @@ namespace Dominoes\Domain;
 
 class Dominoes
 {
+    public const SET_SIZE = 6;
+
     /**
      * @return Tile[]
      */
     public function generateTiles(): array
     {
+        $tiles = [];
+        for ($right = 0; $right < self::SET_SIZE + 1; $right++) {
+            for ($left = $right; $left < self::SET_SIZE + 1; $left++) {
+                $tiles[] = new Tile($left, $right);
+            }
+        }
+
+        return $tiles;
     }
 }
